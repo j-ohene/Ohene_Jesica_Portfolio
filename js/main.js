e58b036f24c8e84203641f06e760d4845d09e435
j-ohene <jesicaohene1@gmail.com>
2023-12-10 10:34:28 -0500

    scrolltrigger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //variables//
-
 const playerCon = document.querySelector("#player-container");
 const player = document.querySelector("video");
 //if theres more thsn 1 video document.querySelectorAll("video")//
@@ -29,9 +28,37 @@ function changeVolume(){
     player.volume = volumeSlider.value;
     console.log(volumeSlider.value);
 }
+
 //event listeners//
 
 playButton.addEventListener("click", playVideo);
 stopButton.addEventListener("click", stopVideo);
 pauseButton.addEventListener("click", pauseVideo);
 volumeSlider.addEventListener("change", changeVolume);
+(() => {
+    let tl = gsap.timeline({
+        // yes, we can add it to an entire timeline!
+        scrollTrigger: {
+          trigger: ".front-end-background",
+          pin: true, 
+          start: "top top", 
+          end: "+=500", //
+          scrub: 1, // 
+          snap: {
+            snapTo: "labels", 
+            duration: { min: 0.2, max: 3 },
+            delay: 0.2, 
+            ease: "power1.inOut", 
+          },
+        },
+      });
+      
+      // add animations and labels to the timeline
+      tl.addLabel("start")
+        .from(".gear", { scale: 0.3, rotation: 45, autoAlpha: 0 })
+        .addLabel("color")
+        .from(".gear")
+        .addLabel("spin")
+        .to(".gear", { rotation: 360 })
+        .addLabel("end");
+    })();
