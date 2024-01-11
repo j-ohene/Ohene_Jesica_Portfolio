b5e53226c9b4275c37a11e73b7b5a7e172d17d66
j-ohene <jesicaohene1@gmail.com>
2024-01-11 17:53:34 -0500

    fixed the scrollTrigger!

--- js/main.js
+++ js/main.js
@@ -45,7 +45,9 @@ stopButton.addEventListener("click", stopVideo);
 pauseButton.addEventListener("click", pauseVideo);
 volumeSlider.addEventListener("change", changeVolume);
 
-gsap.registerPlugin(scrollTrigger);
+
+
+gsap.registerPlugin(ScrollTrigger);
 
     let tl = gsap.timeline({
         // yes, we can add it to an entire timeline!
@@ -53,12 +55,12 @@ gsap.registerPlugin(scrollTrigger);
           trigger: ".front-end-background",
           pin: true, 
           start: "top top", 
-          end: "+=250", //
+          end: "+=500", //
           scrub: 1, // 
           snap: {
             snapTo: "labels", 
             duration: { min: 0.2, max: 3 },
-            delay: 0.2, 
+            delay: 0, 
             ease: "power1.inOut", 
           },
         },
@@ -66,11 +68,11 @@ gsap.registerPlugin(scrollTrigger);
       
       // add animations and labels to the timeline
       tl.addLabel("start")
-        .from(".gear", { scale: 0.3, rotation: 45, autoAlpha: 0 })
+        .from(".gear", { scale: 0.8, rotation: 180, autoAlpha: 0 })
         .addLabel("color")
         .from(".gear")
         .addLabel("spin")
-        .to(".gear", { rotation: 800 })
+        .to(".gear", { rotation: 360 })
         .addLabel("end");
       
   
