654b86bdad815e74cc0342daef05e2951eddefa4
j-ohene <jesicaohene1@gmail.com>
2024-01-16 09:02:34 -0500

    greensock to motion

--- js/main.js
+++ js/main.js
@@ -74,7 +74,31 @@ gsap.registerPlugin(ScrollTrigger);
         .addLabel("spin")
         .to(".gear", { rotation: 360 })
         .addLabel("end");
+
+        let tlm = gsap.timeline({
+           scrollTrigger: {
+          trigger: ".motion-background",
+          pin: true, 
+          start: "top top", 
+          end: "+=300", //
+          scrub: 1, // 
+          snap: {
+            snapTo: "labels", 
+            duration: { min: 0.2, max: 3 },
+            delay: 0, 
+            ease: "power1.inOut", 
+          },
+        },
+      });
       
+      // add animations and labels to the timeline
+      tlm.addLabel("start")
+        .from(".clapper", { scale: 0.8, rotation: 30, autoAlpha: 0 })
+        .addLabel("color")
+        .from(".clapper")
+        .addLabel("spin")
+        .to(".clapper", { rotation: 60 })
+        .addLabel("end");
   
    
    
