cecedf9c6089a3717bef6138c013cd0151dea8d9
j-ohene <jesicaohene1@gmail.com>
2024-02-23 20:07:36 -0500

    video player module

--- js/main.js
+++ js/main.js
@@ -1,47 +1,8 @@
 
 import {scrollTrigger} from "./modules/scrolltrigger.js";
+import {videoPlayer} from "./modules/videoPlayer.js"
 
-//const playerCon = document.querySelector("#player-container");
-//const player = document.querySelector("video");
-//const videoControls = document.querySelector("#video-controls");
-//const playButton = document.querySelector("#play-button");
-//const pauseButton = document.querySelector ("#pause-button");
-//const stopButton = document.querySelector ("#stop-button");
-//const volumeSlider = document.querySelector("#change-vol");
-//const fullScreen = document.querySelector ("#full-screen");
-//functions//
-//player.controls = false;
-//JS loaded, now lets hide the default controls  
-//player.controls = false;
-//videoControls.classList.remove('hidden');
-
-//function playVideo() {
-  //  player.play();
-//}
-//function pauseVideo(){
-    //player.pause();
-//}
-//function stopVideo(){
- //   player.pause();
-   // player.currentTime = 1;
-    //current time is associated with seconds so it would pause then go to the first second//
-//}
-//function changeVolume(){
-    //player.volume = volumeSlider.value;
-  //  console.log(volumeSlider.value);
-//}
-//event listeners//
-
-
-
-//to find events 
-
-//event listeners//
-
-//playButton.addEventListener("click", playVideo);
-//stopButton.addEventListener("click", stopVideo);
-//pauseButton.addEventListener("click", pauseVideo);
-//volumeSlider.addEventListener("change", changeVolume);
 
 
 scrollTrigger();
+videoPlayer();
