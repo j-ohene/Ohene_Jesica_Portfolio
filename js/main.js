b89226299c321ddb18fe612caa2dfcfc34828cbf
j-ohene <jesicaohene1@gmail.com>
2024-02-25 22:17:38 -0500

    changing slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-
 import {scrollTrigger} from "./modules/scrolltrigger.js";
-import {videoPlayer} from "./modules/videoPlayer.js"
+import {videoPlayer} from "./modules/videoPlayer.js";
+import{cardSlider} from "./modules/cardslider.js";
 
 
 
-scrollTrigger();
-videoPlayer();
+
+if(document.body.dataset==='home'){
+    videoPlayer();
+    cardSlider();
+  } else if (document.body.dataset.page==='gallery'){
+    scrollTrigger();
+  };
